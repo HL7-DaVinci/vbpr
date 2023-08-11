42a95895e719e6da62e0c46e4cdb52726fc5d6f3
Chris Lance <climber2039@gmail.com>
2023-08-11 07:35:09 -0500

    Housekeeping

--- bin/probe-env.sh
+++ bin/probe-env.sh
@@ -3,6 +3,10 @@
 BIN_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 . ${BIN_DIR}/../.env
 
+if [ -f ${BIN_DIR}/../.env.local ]; then 
+    . ${BIN_DIR}/../.env.local
+fi
+
 if [ -z ${CONTAINER_CLI} ]; then
     if command -v docker &> /dev/null; then
         export CONTAINER_CLI=docker
